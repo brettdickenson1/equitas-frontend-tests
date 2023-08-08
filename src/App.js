951ba2650c9a0e248ec401935ae46611a0eabf13
Brett Dickenson <46332464+brettdickenson1@users.noreply.github.com>
2023-08-08 20:12:31 +0100

    equitas-frontend-task-complete

--- src/App.js
+++ src/App.js
@@ -1,150 +1,202 @@
-import './App.css';
-import {useState, useEffect} from "react";
+import "./App.css";
+import { useState, useEffect } from "react";
 import Launches from "./components/Launches";
-import {Button, Container} from "@mui/material";
+import { Button, Container } from "@mui/material";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const apiUrl = "https://api.spacexdata.com/v4/launches/query";
 
-    const [data, setData] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1)
-    const apiUrl = 'https://api.spacexdata.com/v4/launches/query';
-
-    function getQueryBody(pageNumber) {
-        return  {
-            query: {
-                upcoming: false,
-                success: true
+  function getQueryBody(pageNumber) {
+    return {
+      query: {
+        upcoming: false,
+        success: true,
+      },
+      options: {
+        page: pageNumber,
+        select: {
+          id: 1,
+          name: 2,
+          links: 3,
+          date_utc: 4,
+          flight_number: 5,
+        },
+        populate: [
+          {
+            path: "rocket",
+            select: {
+              id: 1,
+              name: 2,
+              type: 3,
+              description: 4,
+              height: 5,
+              diameter: 6,
+              mass: 7,
+              flickr_images: 8,
             },
-            options: {
-                page: pageNumber,
-                select: {
-                    id: 1,
-                    name: 2,
-                    links: 3,
-                    date_utc: 4,
-                    flight_number: 5,
-                },
-                populate: [
-                    {
-                        path: 'rocket',
-                        select: {
-                            id: 1,
-                            name: 2,
-                            type: 3,
-                            description: 4,
-                            height: 5,
-                            diameter: 6,
-                            mass: 7,
-                            flickr_images: 8,
-                        },
-                    },
-                    {
-                        path: 'crew',
-                        select: {
-                            id: 1,
-                            name: 2,
-                            agency: 3,
-                            image: 4,
-                        },
-                    },
-                    {
-                        path: 'payloads',
-                        select: {
-                            id: 1,
-                            name: 2,
-                            type: 3,
-                            orbit: 4,
-                            reference_system: 5,
-                            regime: 6
-                        }
-                    },
-                    {
-                        path: 'capsules',
-                        select: {
-                            id: 1,
-                            type: 2,
-                            status: 3,
-                            serial: 4
-                        }
-                    },
-                    {
-                        path: 'launchpad',
-                        select: {
-                            id: 1,
-                            name: 2,
-                            full_name: 3,
-                            locality: 4,
-                            region: 5,
-                            latitude: 6,
-                            longitude: 7,
-                            details: 8
-                        }
-                    }
-                ],
-                sort: {
-                    flight_number: 'desc',
-                },
+          },
+          {
+            path: "crew",
+            select: {
+              id: 1,
+              name: 2,
+              agency: 3,
+              image: 4,
             },
-        };
-
-    }
-
-    const fetchData = async (pageNumber) => {
-        try {
-            const response = await fetch(apiUrl, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(getQueryBody(pageNumber)),
-            });
-
-            if (!response.ok) {
-                console.log('Network response was not ok');
-            }
-
-            const responseData = await response.json();
-            console.log(responseData)
-            setData(responseData);
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
+          },
+          {
+            path: "payloads",
+            select: {
+              id: 1,
+              name: 2,
+              type: 3,
+              orbit: 4,
+              reference_system: 5,
+              regime: 6,
+            },
+          },
+          {
+            path: "capsules",
+            select: {
+              id: 1,
+              type: 2,
+              status: 3,
+              serial: 4,
+            },
+          },
+          {
+            path: "launchpad",
+            select: {
+              id: 1,
+              name: 2,
+              full_name: 3,
+              locality: 4,
+              region: 5,
+              latitude: 6,
+              longitude: 7,
+              details: 8,
+            },
+          },
+        ],
+        sort: {
+          flight_number: "desc",
+        },
+      },
     };
+  }
 
-    useEffect(() => {
-        fetchData(currentPage);
-    }, []);
+  const fetchData = async (pageNumber) => {
+    try {
+      const response = await fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(getQueryBody(pageNumber)),
+      });
 
-    const nextPage = () => {
-        setCurrentPage(currentPage + 1)
-        fetchData(currentPage + 1)
+      if (!response.ok) {
+        console.log("Network response was not ok");
+      }
+
+      const responseData = await response.json();
+      console.log(responseData);
+      setData(responseData);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-    const prevPage = () => {
-        setCurrentPage(currentPage - 1)
-        fetchData(currentPage - 1)
-    }
+  };
 
-    return (
-        <div>
+  useEffect(() => {
+    fetchData(currentPage);
+  }, []);
 
-            <Container>
-                <p>Total Launches: {data["totalDocs"]}</p>
-                {data["docs"] ? (
-                    <div>
-                        <Launches launches={data["docs"]}/>
-                        <p>Page {data["page"]} / {data["totalPages"]} </p>
-                        <Button variant="outlined" onClick={prevPage} disabled={currentPage === 1}>Prev Page</Button>
-                        <Button variant="outlined" onClick={nextPage} disabled={currentPage === data["totalPages"]}>Next
-                           Page</Button>
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1);
+    fetchData(currentPage + 1);
+  };
+  const prevPage = () => {
+    setCurrentPage(currentPage - 1);
+    fetchData(currentPage - 1);
+  };
 
-                   </div>
-                ) : (
-                    <div>Loading...</div>
-                )}
+  const firstPage = () => {
+    setCurrentPage(1);
+    fetchData(1);
+  };
 
-            </Container>
+  const lastPage = () => {
+    setCurrentPage(19);
+    fetchData(19);
+  };
 
-        </div>
-    );
+  const buttonStyles = {
+    fontSize: 12,
+    height: 30,
+  };
+
+  return (
+    <div className="container">
+      <Container>
+        <p className="totalLaunches">Total Launches: {data["totalDocs"]}</p>
+        {data["docs"] ? (
+          <div className="innerContainer">
+            <Launches launches={data["docs"]} />
+            <p
+              style={{
+                color: "white",
+                fontWeight: 900,
+                fontSize: 20,
+                margin: 10,
+                float: "right",
+              }}
+            >
+              Page {data["page"]} / {data["totalPages"]}{" "}
+            </p>
+            <div className="buttonContainer">
+              <Button
+                style={buttonStyles}
+                variant="contained"
+                onClick={firstPage}
+                disabled={currentPage === 1}
+              >
+                <SkipPreviousIcon />
+              </Button>
+              <Button
+                style={buttonStyles}
+                variant="contained"
+                onClick={prevPage}
+                disabled={currentPage === 1}
+              >
+                Prev Page
+              </Button>
+              <Button
+                style={buttonStyles}
+                variant="contained"
+                onClick={nextPage}
+                disabled={currentPage === data["totalPages"]}
+              >
+                Next Page
+              </Button>
+              <Button
+                style={buttonStyles}
+                variant="contained"
+                onClick={lastPage}
+                disabled={currentPage === data["totalPages"]}
+              >
+                <SkipNextIcon />
+              </Button>
+            </div>
+          </div>
+        ) : (
+          <div className="loading">Loading...</div>
+        )}
+      </Container>
+    </div>
+  );
 }
 export default App;
--- src/components/Launch/index.js
+++ src/components/Launch/index.js
@@ -1,17 +1,31 @@
-import styles from './launch.module.css'
-import moment from 'moment'
+import styles from "./launch.module.css";
+import moment from "moment";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import LocalAirportIcon from "@mui/icons-material/LocalAirport";
+import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 
-const Launch = ({props}) => {
-
-    return (
-        <div>
-            <div className={styles.launchTitle}>{props.name}</div>
-            <div>Id: {props.id}</div>
-            <div>Flight Number: {props.flight_number}</div>
-            <div>Launch Date: {moment(props.date_utc).format('MMMM Do YYYY, h:mm:ss a')}</div>
+const Launch = ({ props }) => {
+  return (
+    <div className={styles.launchCard}>
+      <div className={styles.launchTitle}>{props.name}</div>
+      <div className={styles.bottomHalf}>
+        <div className={styles.launchID}>
+          <CheckCircleOutlineIcon style={{ marginRight: 10, height: 20 }} />
+          <span style={{ fontWeight: 900 }}>ID:</span> {props.id}
         </div>
-    )
+        <div className={styles.flightNumber}>
+          <LocalAirportIcon style={{ marginRight: 10, height: 20 }} />
+          <span style={{ fontWeight: 900 }}>Flight Number:</span>{" "}
+          {props.flight_number}
+        </div>
+        <div className={styles.launchDate}>
+          <RocketLaunchIcon style={{ marginRight: 10, height: 20 }} />
+          <span style={{ fontWeight: 900 }}>Launch Date:</span>{" "}
+          {moment(props.date_utc).format("MM.DD.YYYY")}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-}
-
-export default Launch
+export default Launch;
